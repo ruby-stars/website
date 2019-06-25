711259229c526df74f6496242bacf4d4085c3059
Eva Stolz <evasmail@gmail.com>
2019-06-25 20:28:58 +0200

    Define contact#create action and configure route

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,4 +5,21 @@ class ContactController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      #flash[:notice] = "Thank you!"
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def contact_params
+    params.permit(:firstName, :email, :message)
+  end
 end
