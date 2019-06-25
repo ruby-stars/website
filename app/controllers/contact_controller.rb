f0bc2b16c9d372d57540a5855781f42c29e8b97a
Eva Stolz <evasmail@gmail.com>
2019-06-25 19:55:26 +0200

    Create form and model for new contact

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,8 @@
 class ContactController < ApplicationController
   def index
   end
+
+  def new
+    @contact = Contact.new
+  end
 end
---
+++ db/migrate/20190625173425_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :firstName
+      t.string :email
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
