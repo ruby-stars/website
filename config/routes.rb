c56934a5456522439d7f7206b35033e061aecb3a
Julia Wolf <juliaingeburgwolf@gmail.com>
2019-11-12 18:56:41 +0100

    Set up sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'about#index'
   get 'contact', to: 'contact#new'
   post 'contact', to: 'contact#create'
 
-  #resources :contacts
+  # resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
